6f200465ed19cf841e761b534604d12f955ce517
Matthew Casperson <matthewcasperson@gmail.com>
2020-01-02 16:50:09 +1000

    Added compile script for MacOS/Linux

--- compile.sh
+++ compile.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+# Install build-lambda-zip with:
+# go get -u github.com/aws/aws-lambda-go/cmd/build-lambda-zip
+
 GOARCH=amd64 GOOS=linux go build
+${GOPATH}/bin/build-lambda-zip --output ./AWSLambdaGo.zip ./AWSLambdaGo
