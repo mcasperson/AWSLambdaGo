a14715905edf115b21362413b2c719e6aeff70de
Matthew Casperson <matthewcasperson@gmail.com>
2021-12-10 10:49:14 +1000

    Update compile.ps1

--- compile.ps1
+++ compile.ps1
@@ -1,5 +1,5 @@
 # Install build-lambda-zip with:
-# go.exe get -u github.com/aws/aws-lambda-go/cmd/build-lambda-zip
+# go.exe install github.com/aws/aws-lambda-go/cmd/build-lambda-zip@latest
 
 $env:GOARCH="amd64"
 $env:GOOS="linux"
